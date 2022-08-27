e80c7bb6a91005f734544485d94d1ffb58b55398
muhammad.hashim@dastgyr.com <muhammad.hashim@dastgyr.com>
2022-08-27 23:51:49 +0500

    Fixed connection value in knexFile.js

--- knexfile.js
+++ knexfile.js
@@ -59,13 +59,7 @@
  
  production: {
    client: 'postgresql',
-   connection: {
-     host: process.env.DB_HOST,
-     database: process.env.DB_NAME,
-     user: process.env.DB_USER,
-     password: process.env.DB_PASSWORD,
-     charset: 'utf8'
-   },
+   connection: process.env.DATABASE_URI,
    pool: {
      min: 2,
      max: 10
