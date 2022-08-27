51e46a6ecb31f0198ecbceaab1569b16eb2d30d9
muhammad.hashim@dastgyr.com <muhammad.hashim@dastgyr.com>
2022-08-28 00:58:04 +0500

    --allow-empty

--- src/config/db.js
+++ src/config/db.js
@@ -16,12 +16,11 @@ module.exports = {
         "dialect": knexConfig.staging.client,
     },
     "production": {
-        // "username": knexConfig.production.connection.user,
-        // "password": knexConfig.production.connection.password,
-        // "database": knexConfig.production.connection.database,
-        // "host": knexConfig.production.connection.host,
-        // "dialect": knexConfig.production.client,
-        "connection": knexConfig.production.connection
+        "username": knexConfig.production.connection.user,
+        "password": knexConfig.production.connection.password,
+        "database": knexConfig.production.connection.database,
+        "host": knexConfig.production.connection.host,
+        "dialect": knexConfig.production.client,
     }
   }
   
