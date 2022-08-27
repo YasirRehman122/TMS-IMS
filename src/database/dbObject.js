b1d678dec8d7cd385aba8f37f185cde1ed8fdff0
muhammad.hashim@dastgyr.com <muhammad.hashim@dastgyr.com>
2022-08-28 02:27:37 +0500

    --allow-empty

--- src/database/dbObject.js
+++ src/database/dbObject.js
@@ -12,7 +12,6 @@ let db = knex({
     user: dbConfig[process.env.NODE_ENV].username,
     password: dbConfig[process.env.NODE_ENV].password,
     database: dbConfig[process.env.NODE_ENV].database,
-    // ssl:dbConfig[process.env.NODE_ENV].ssl,
     // port: 5444,
     pool: {
       autostart: true,
@@ -20,6 +19,9 @@ let db = knex({
       min: 2,
       propagateCreateError: false
     }
+  },
+  ssl: {    /* <----- Add SSL option */
+    rejectUnauthorized: false,
   },
 //   log: {
 //     warn(message) {
